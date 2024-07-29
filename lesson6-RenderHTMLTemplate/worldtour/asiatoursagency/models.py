706bf1891d57cbd8fde56ce240afef99e1ae8708
mark <markpackham1@gmail.com>
2024-07-29 14:50:59 +0100

    Finish off constructor
    https://youtu.be/RozHfswVZYE?si=GlkB6ES9GhHXfhG-&t=34

--- lesson6-RenderHTMLTemplate/worldtour/asiatoursagency/models.py
+++ lesson6-RenderHTMLTemplate/worldtour/asiatoursagency/models.py
@@ -8,5 +8,10 @@ class Tour(models.Model):
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
 
+    # This is a string representation of the tours
     def __str__(self):
-        return (f"ID:{self.id}: FROM {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs £{self.price}")
+        return (f"ID:{self.id}: From {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs ${self.price}")
+
+    
+    
+    
