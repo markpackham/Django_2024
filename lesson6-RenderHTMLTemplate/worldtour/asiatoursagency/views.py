a2d81cca63661e450dfd073756979d879fdf4106
mark <markpackham1@gmail.com>
2024-08-14 08:29:04 +0100

    Loop through the tours
    https://youtu.be/RozHfswVZYE?si=Z1hoi309iGEFnpk2&t=494

--- lesson6-RenderHTMLTemplate/worldtour/asiatoursagency/views.py
+++ lesson6-RenderHTMLTemplate/worldtour/asiatoursagency/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Tour
 
 # Create your views here.
 def index(request):
+    tours = Tour.objects.all()
+    context = {'tours':tours}
     # With linux & unix devices use "/"
     # return render(request, 'tours/index.html')
-    return render(request, 'tours\index.html')
+    return render(request, 'tours\index.html',context)
