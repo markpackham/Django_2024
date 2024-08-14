0fbb401562e0e831f9407f90e41ef5f0635ea4f3
mark <markpackham1@gmail.com>
2024-08-14 09:09:39 +0100

    Demo addition of superuser
    https://youtu.be/xc363xZGxXI?si=i9JZyMlVEvVIl5i0&t=251

---
+++ lesson7-AdminDashboard/worldtour/asiatoursagency/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Tour
+# Register your models here.
+# http://127.0.0.1:8000/admin/
+admin.site.register(Tour)
---
+++ lesson7-AdminDashboard/worldtour/asiatoursagency/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Tour(models.Model):
+    # we need a origin country, we need a destination, number of nights, and we need the price for that tour.
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return (f"ID:{self.id}: FROM {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs £{self.price}")
