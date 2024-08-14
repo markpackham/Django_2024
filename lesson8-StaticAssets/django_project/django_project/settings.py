c8765b60c42105544d87bc4cb11659836870d174
mark <markpackham1@gmail.com>
2024-08-14 10:47:35 +0100

    Turn into a tuple

--- lesson8-StaticAssets/django_project/django_project/settings.py
+++ lesson8-StaticAssets/django_project/django_project/settings.py
@@ -121,7 +121,9 @@ STATIC_URL = 'static/'
 
 #MEDIA_URL = 'images/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
