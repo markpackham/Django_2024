17f6a3e96780fd0f6248e6589b95359b47e19ed9
mark <markpackham1@gmail.com>
2024-08-15 09:56:47 +0100

    Add homepage view function
    https://youtu.be/WQISlkg_IZk?si=DLRlFgk-lJKDjJIW&t=710

--- lesson9-DjangoForms/form_project/form_app/urls.py
+++ lesson9-DjangoForms/form_project/form_app/urls.py
@@ -1,5 +1,2 @@
 from django.urls import path
-from .form import ContactForm
-
-def home_view(request):
-    return render(request, 'form_app/')
+from . import views
--- lesson9-DjangoForms/form_project/form_app/views.py
+++ lesson9-DjangoForms/form_project/form_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .form import ContactForm
 
 # Create your views here.
+
+# The homepage view function
+def home_view(request):
+    return render(request, 'form_app/home.html')
