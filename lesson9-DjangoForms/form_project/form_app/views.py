2254c5af6e726e08d48024f5e945e887e1a62e52
mark <markpackham1@gmail.com>
2024-08-15 10:08:30 +0100

    Add contact success function
    https://youtu.be/WQISlkg_IZk?si=hwSbqtzrptt4rSJm&t=1029

--- lesson9-DjangoForms/form_project/form_app/views.py
+++ lesson9-DjangoForms/form_project/form_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import ContactForm
 
 # Create your views here.
@@ -6,3 +6,19 @@ from .form import ContactForm
 # The homepage view function
 def home_view(request):
     return render(request, 'form_app/home.html')
+
+# Define the contact_view to handle the contact form
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'form_app/contact.html', context)
+
+# Define the contact_success_view function to handle successful posts
+def contact_success_view(request):
+    return render(request, 'form_app/contact-success.html')
