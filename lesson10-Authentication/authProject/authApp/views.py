d14fa60125b82254857b15aae9ccf9a04c594c28
mark <markpackham1@gmail.com>
2024-08-16 13:16:53 +0100

    Import django auth classes for the views file
    https://youtu.be/0gAFW4UxsZg?si=Ngdu2Tcs-Ld0viO3&t=771

--- lesson10-Authentication/authProject/authApp/views.py
+++ lesson10-Authentication/authProject/authApp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
