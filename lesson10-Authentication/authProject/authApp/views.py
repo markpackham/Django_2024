4b69fef0ac33b817aa89580eff1e37204bfa7014
mark <markpackham1@gmail.com>
2024-08-16 13:20:34 +0100

    Add skeleton of view functions
    https://youtu.be/0gAFW4UxsZg?si=-1ws3WoDzs2mvyT_&t=890

--- lesson10-Authentication/authProject/authApp/views.py
+++ lesson10-Authentication/authProject/authApp/views.py
@@ -1,5 +1,29 @@
+# To handle views and redirects
 from django.shortcuts import render, redirect
+# To Import auth functions form Django
 from django.contrib.auth import authenticate, login, logout
+# The login_required decorator to protect views
 from django.contrib.auth.decorators import login_required
+# For class-based views[CBV]
 from django.contrib.auth.mixins import LoginRequiredMixin
+# For class-based views[CBV]
+from django.views import View
+#  Import the User class (model)
+from django.contrib.auth.models import User
+# Import the RegisterForm from forms.py
+from .forms import RegisterForm
 
+def register_view(request):
+    pass
+
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
+
+# Home view
+# Using the decorator
+@login_required
+def home_view(request):
+    pass
