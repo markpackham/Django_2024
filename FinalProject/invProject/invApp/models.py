abd1b86331ceda848d95676dde0a752302d1c710
mark <markpackham1@gmail.com>
2024-08-19 11:59:17 +0100

    Migrate product
    https://youtu.be/wQPaXedytlg?si=5bXON55ViKFKlh-Q&t=601

--- FinalProject/invProject/invApp/models.py
+++ FinalProject/invProject/invApp/models.py
@@ -4,7 +4,10 @@ class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     # Stock Keeping Unique (each product must be unique)
-    sku = models.CharField(max_length=100)
-    price = models.CharField(max_length=100)
-    quantity = models.CharField(max_length=100)
+    sku = models.CharField(max_length=50, unique=True)
+    price = models.FloatField()
+    quantity = models.IntegerField()
     supplier = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- FinalProject/invProject/invProject/settings.py
+++ FinalProject/invProject/invProject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'invApp',
     'crispy_forms',
-    'crispy_bootstrap5'
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
