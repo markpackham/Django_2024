da75ff49d6a782bd3274a9850c9b3b699ba735fa
mark <markpackham1@gmail.com>
2024-08-19 11:04:30 +0100

    Add product fields
    https://youtu.be/wQPaXedytlg?si=dlsz9FAENwrPIppC&t=449

--- FinalProject/invProject/invApp/models.py
+++ FinalProject/invProject/invApp/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    # Stock Keeping Unique (each product must be unique)
+    sku = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100)
+    supplier = models.CharField(max_length=100)
