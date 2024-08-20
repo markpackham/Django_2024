02dfd0f7a65aaac0e19d5406afc1d455d5c4119b
mark <markpackham1@gmail.com>
2024-08-20 07:26:21 +0100

    Add product create view function
    https://youtu.be/jyjytSjqII4?si=Y3OMy6XqfbEkFb65&t=397

--- FinalProject/invProject/invApp/views.py
+++ FinalProject/invProject/invApp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ProductForm
 from .models import Product
 
 def home_view(request):
     return render(request, 'invApp/home.html')
+
+def product_create_view(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+        # Redirect if it is not a POST request
+    return render(request, 'invApp/product_form.html', {'form':form})
